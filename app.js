71e8a35a1362065f6d0159866ae43c3cbca495a5
Daisy <qoo8857300@gmail.com>
2021-04-02 00:16:53 +0800

    add mongoose

--- app.js
+++ app.js
@@ -2,10 +2,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+
+mongoose.connect('mongodb://localhost/My_restaurant',{useNewUrlParser: true ,useUnifiedTopology: true})
+
+const db = mongoose.connection
+
+db.on('error', () =>{
+    console.log('mongodb error!')
+})
+
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
 
 // routes setting
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
